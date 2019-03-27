28ffdac7ecec25088e620aa198449fcec0c51506
lucasprimo375 <lucasprimo375@hotmail.com>
2019-03-27 12:22:54 -0300

    first version of quicksort with bfprt selection

---
+++ quicksort/bfprt.cc
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+float* input_vector(int* size);
+void quick_sort(float* vector, int initial_index, int last_index);
+int bfprt_partition(float* vector, int initial_index, int last_index, int i_index);
+int hoare_selection(float* vector, int initial_index, int last_index, int i_index);
+void swap(float* v1, float* v2);
+void print_vector(float* vector, int size);
+
+int main(){
+	int size;
+	float* vector = input_vector(&size);
+
+	std::cout << "printing vector before ordering" << std::endl;
+
+	print_vector(vector, size);
+
+	bfprt_partition(vector, 0, size-1, std::floor(size/2));
+
+	std::cout << "printing vector after ordering" << std::endl;
+
+	print_vector(vector, size);
+
+	return 0;
+}
+
+int bfprt_partition(float* vector, int initial_index, int last_index, int i_index){
+	int size = last_index - initial_index + 1;
+
+	if(size <= 5){
+		return hoare_selection(vector, initial_index, last_index, std::floor(size/2));
+	}
+
+
+	int median_position = initial_index;
+	for(int i = initial_index; i <= last_index; i = i + 5){
+		int median_index = bfprt_partition(vector, i, std::min(i + 4, last_index), i_index);
+		std::cout << "median_index: " << median_index << std::endl;
+		swap(&vector[median_index], &vector[median_position]);
+		median_position++;
+	}
+
+	int last_median_index = median_position - 1;
+
+	std::cout << "last_median_index: " << last_median_index << std::endl;
+
+	return 0;
+}
+
+void quick_sort(float* vector, int initial_index, int last_index){
+	int size = last_index - initial_index + 1;
+	if(initial_index < last_index){
+		int pivot_index = bfprt_partition(vector, initial_index, last_index, std::floor(size/2));
+		quick_sort(vector, initial_index, pivot_index - 1);
+		quick_sort(vector, pivot_index + 1, last_index);
+	}
+}
+
+int hoare_selection(float* vector, int initial_index, int last_index, int i_index){
+	std::cout << "analisando " << initial_index << " a " << last_index << std::endl;
+	int bla;
+	std::cin >> bla;
+	if(initial_index == last_index) return last_index;
+
+	float pivot = vector[last_index];
+
+	int j = initial_index;
+	for(int i=initial_index; i<last_index; i++){
+		if(vector[i] <= pivot){
+			swap(&vector[i], &vector[j]);
+			j++;
+		}
+	}
+
+	swap(&vector[j], &vector[last_index]);
+
+	if(j == i_index) return j;
+
+	if(i_index > j) return hoare_selection(vector, j+1, last_index, i_index);
+
+	// i_index < j
+	return hoare_selection(vector, initial_index, j-1, i_index);
+}
+
+float* input_vector(int* size){
+	/*std::cout << "input vector size: ";
+	std::cin >> *size;*/
+	*size = 10;
+
+	float* aux = new float[*size];
+
+	for(int i=0; i<*size; i++){
+		/*std::cout << "input element " << i+1 << ": ";
+		std::cin >> aux[i];*/
+		aux[i] = (*size) - i;
+	}
+
+	//std::cout << std::endl;
+
+	return aux;
+}
+
+void print_vector(float* vector, int size){
+	for(int i=0; i<size; i++){
+		if(i != size - 1){
+			std::cout << vector[i] << ", ";
+		} else {
+			std::cout << vector[i] << "." << std::endl;
+		}
+	}
+
+	std::cout << std::endl;
+}
+
+void swap(float* v1, float* v2){
+	float temp = *v1;
+	*v1 = *v2;
+	*v2 = temp;
+}
